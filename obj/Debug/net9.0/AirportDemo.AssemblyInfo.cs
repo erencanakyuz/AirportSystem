c1a5e8d7a6a8efcfa98da7199ec115d78fa076da
erencanakyuz <erencakyuz@gmail.com>
2025-02-03 00:01:59 +0300

    Not working good(for UI)
    it is just saving the ui of flight page but not working good

--- obj/Debug/net9.0/AirportDemo.AssemblyInfo.cs
+++ obj/Debug/net9.0/AirportDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AirportDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+520ba23863d522f82cc136198e06177ee658abe7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b28d6cdef29e18fd42ed8aea17aabe0a283ee0b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AirportDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AirportDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
