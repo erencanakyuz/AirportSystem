c2106652387df151c5e1817a6c46fe01d429ae84
erencanakyuz <erencakyuz@gmail.com>
2025-02-02 22:27:51 +0300

    Login page fixed 0.05
    Login page layout fixed

--- obj/Debug/net9.0/AirportDemo.AssemblyInfo.cs
+++ obj/Debug/net9.0/AirportDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AirportDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6d0c2dee2e44afec37bac6b4856ec0958e91099")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a9983b534c622962276b9135a722acd38161e79")]
 [assembly: System.Reflection.AssemblyProductAttribute("AirportDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AirportDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
